f13e85d31e4d68191ab653a15bb4c98589f80b1b
Hans-Peter Martini <hapemartini@gmail.com>
2020-12-21 20:42:36 +0100

    init and add basic template/functionality

---
+++ public/js/app.js
@@ -0,0 +1,13 @@
+const requestModal = document.querySelector(".new-request");
+const requestLink = document.querySelector(".add-request");
+
+// open request modal
+requestLink.addEventListener("click", () => {
+  requestModal.classList.add("open");
+});
+
+requestModal.addEventListener("click", (e) => {
+  if (e.target.classList.contains("new-request")) {
+    requestModal.classList.remove("open");
+  }
+});
